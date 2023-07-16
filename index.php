f78d857e4b17a52ea7d1747f46c757754146c72f
Ritesh Datir <riteshdatir060@gmail.com>
2023-07-16 17:33:43 +0530

    updated index.php

--- index.php
+++ index.php
@@ -16,14 +16,26 @@
 			 <li>Kharadi</li>
 			 <li>Sabka-somu</li>
 			 </ul>
-			 <h2>Main attarction at night after utsav, Panvel</h2>
+			 
+			 <h2> Mukhya akarshan at night after utsav, Panvel</h2>
 			  <style>
 	  h2{
 		  color :purple;
 	  }
 	   </style>
 			 
-			 
+		<ul>
+              <li> khaicha Menu </li>
+              <li> Chicken , Mutton, daru ani Pori </li>
+              </ul>
+
+      <h3> Tip: Krupaya krun konhi hee thaht lavdya shivay kahi anu naya </h3>
+  
+   <style>
+h3{
+       color : pink;
+}
+  </style>	   
 		
 			 
 			</body>
