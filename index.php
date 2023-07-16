ecfba02019b468e54bc1659a053dd9d451220bd4
Ritesh Datir <riteshdatir060@gmail.com>
2023-07-16 17:08:03 +0530

    updated index.php

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <html>
      <head>
+	 <style>
+	  h1{
+		  color :red;
+	  }
+	  </style>
 	        <title> Welcome to aws cicd</title>
 			</head>
 			<body>
 			<h1>Hello Radhe Krishna...!</h1>
-			
+			<ul>
+			 <li>Ritesh</li>
+			 <li>Ajinkya</li>
+			 <li>Bandgoal</li>
 			</body>
 			</html>
