34a2c942128fce2208eade955c9a30311baaa4dc
Ritesh Datir <riteshdatir060@gmail.com>
2023-07-16 17:48:25 +0530

    updated index.php

--- index.php
+++ index.php
@@ -5,37 +5,16 @@
 		  color :red;
 	  }
 	  </style>
-	        <title> Gatari utsav </title>
+	        <title> aws-cicid </title>
 			</head>
 			<body>
-			<h1>People invited to Gatari utsav</h1>
+			<h1>Hello Radhe Krishna</h1>
 			<ul>
 			 <li>Ritesh</li>
 			 <li>Ajinkya</li>
-			 <li>Bandgoal</li>
-			 <li>Kharadi</li>
-			 <li>Sabka-somu</li>
+			
 			 </ul>
 			 
-			 <h2> Mukhya akarshan at night after utsav, Panvel</h2>
-			  <style>
-	  h2{
-		  color :purple;
-	  }
-	   </style>
-			 
-		<ul>
-              <li> khaicha Menu </li>
-              <li> Chicken , Mutton, daru ani Pori </li>
-              </ul>
-
-      <h3> Tip: Krupaya krun konhi hee thaht lavdya shivay kahi anu naya </h3>
-  
-   <style>
-h3{
-       color : Red;
-}
-  </style>	   
 		
 			 
 			</body>
